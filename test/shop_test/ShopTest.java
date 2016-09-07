af51f7f82a3c25c5dbdf5aca6c37909e7b2a342a
Vegh Adam <veghadam1991@gmail.com>
2016-09-07 18:24:23 +0200

    add buyMilk method test

--- test/shop_test/ShopTest.java
+++ test/shop_test/ShopTest.java
@@ -2,6 +2,7 @@ package shop_test;
 
 import java.util.Date;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 import shop.Milk;
 import shop.Shop;
@@ -13,13 +14,17 @@ public class ShopTest {
     String address;
     String owner;
     Milk[] milkbar;
+    Milk milk1 = new Milk(0, "", new Date(), 0, 0);
+    Milk milk2 = new Milk(0, "", new Date(), 0, 0);
+    Milk milk3 = new Milk(0, "", new Date(), 0, 0);
+    
+    @Before
+    public void setUp() {
+	milkbar = new Milk[]{milk1, milk2, milk3};
+    }
     
     @Test
-    public void there_is_five_milk_in_shop() {
-	Milk milk1 = new Milk(0, "", new Date(), 0, 0);
-	Milk milk2 = new Milk(0, "", new Date(), 0, 0);
-	Milk milk3 = new Milk(0, "", new Date(), 0, 0);
-	milkbar = new Milk[]{milk1, milk2, milk3};
+    public void there_is_three_milk_in_shop() {
 	shop = new Shop(name, address, owner, milkbar);
 	assertTrue(shop.isThereAnyMilk());
     }
@@ -29,5 +34,21 @@ public class ShopTest {
 	milkbar = new Milk[]{};
 	shop = new Shop(name, address, owner, milkbar);
 	assertFalse(shop.isThereAnyMilk());
+    }
+    
+    @Test
+    public void buy_milk_if_there_is_any() {
+	shop = new Shop(name, address, owner, milkbar);
+	Milk boughtMilk3 = shop.buyMilk(milk3);
+	assertEquals(milk3, boughtMilk3);
+	Milk boughtMilk2 = shop.buyMilk(milk3);
+	assertEquals(milk2, boughtMilk2);
+    }
+    
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void buy_milk_if_there_is_no_one() {
+	milkbar = new Milk[]{};
+	shop = new Shop(name, address, owner, milkbar);
+	shop.buyMilk(milk1);
     }
 }
